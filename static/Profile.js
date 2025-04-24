083033990613ef03705b20040b5f8d63c81a2464
Your Name <you@example.com>
2025-04-24 17:27:19 +0100

    jii

--- static/Profile.js
+++ static/Profile.js
@@ -1,6 +1,7 @@
 import { navigateTo } from "./index.js";
 
 
+
 export async function FetchProfile() {
     const token = localStorage.getItem("jwt");
     console.log(token);
@@ -21,8 +22,16 @@ if (!token) {
   
   </div>
                  <div class="containere">
+                    <section id="text">
                     <div id="profile"></div>
-                    <h3>Statistiques</h3>
+                    <div id="level" ></div>
+                    <div id="projects"></div>
+                    <div id="auditratio"></div>
+                     </section>
+                     <section id="graphs">
+                    <div id ="audit"></div>
+                    <div id="skils"></div>
+                    </section>
                     <svg id="statsGraph" width="400" height="200"></svg>
                     
                         </div>`
@@ -47,10 +56,37 @@ if (!token) {
             totalDown
             auditRatio
         }
+         audit :transaction(where: { type: { _eq: "up" } }) {
+            amount
+        }
+        xp :transaction_aggregate(where: {type: {_eq: "level"}, event: {object: {name: {_eq: "Module"}}}}
+                                      order_by: {createdAt: desc}
+                                    ) {
+                                      aggregate {
+                                        max {
+                                          amount
+                                        }
+                                      }
+                                    }
         transaction(where: { type: { _eq: "xp" } }) {
             amount
             createdAt
         }
+          USER_AUDITS: 
+        user {
+            auditRatio
+            sucess: audits_aggregate(where: { closureType: { _eq: succeeded } }) {
+                aggregate {
+                    count
+                }
+            }
+            failed: audits_aggregate(where: { closureType: { _eq: failed } }) {
+                aggregate {
+                    count
+                }
+            }
+        }
+    
     }`;
 
     const response = await fetch("https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql", {
@@ -63,15 +99,21 @@ if (!token) {
     });
 
     const data = await response.json();
-    console.log(data);
-    console.log(data.data.user);
+    console.log(data.data);
+    
+    console.log(data.data.xp.aggregate.max.amount);
 
     
 
     document.querySelector(".Profilename").innerHTML+=`<p><strong>${data.data.user[0].login}</strong></p>`
     console.log( document.querySelector(".Profilename"));
     document.querySelector("#profile").innerHTML+=`<p><strong>Welcome, ${data.data.user[0].firstName} ${data.data.user[0].lastName}</strong></p>`
+    document.querySelector("#auditratio").innerHTML+=`<h2>audit ratio </h2> ${roundToOneDecimal(data.data.user[0].auditRatio)}`
+    document.querySelector("#level").innerHTML+=`<h2> your level </h2> ${data.data.xp.aggregate.max.amount}`
     
+
+
+  
     
   //  displayProfile(data.data);
    // drawGraph(data.data.transaction);
@@ -83,6 +125,10 @@ function displayProfile(data) {
         <p><strong>ID :</strong> ${data.user[0].id}</p>
     `;
 }
+function roundToOneDecimal(num) {
+    return Math.round(num * 10) / 10;
+  }
+  
 
 function drawGraph(transactions) {
     const svg = document.getElementById("statsGraph");
